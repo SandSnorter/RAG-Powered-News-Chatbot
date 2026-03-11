6e4b979ddb1af8fb274bc468bebba8eca0ba0eea
SandSnorter <shamkuwarpallavi@gmail.com>
2026-03-11 16:53:27 +0530

    changed files

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,15 +2,11 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center px-6 py-2 bg-black/30 backdrop-blur-none sticky top-0 z-50">
         <div>
-            <a href="/" className="flex items-center transition hover:opacity-80">
+            <a href="/" className="flex items-center">
                 <img src="/assets/logoo.png" alt="logo" className="h-15 w-auto object-contain"/>
-                <span className="font-pacifico text-white/70 text-4xl">Nexus</span>
+                <span className="font-pacifico text-white/70 text-4xl transition hover:text-white">Nexus</span>
             </a>
         </div>
-
-        <ul className="hidden md:flex gap-8">
-          <li>Home</li>
-        </ul>
     </nav>
   )
 }
